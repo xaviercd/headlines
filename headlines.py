932c4782c452f05eadfe07bb7d21811ce78c3e2a
xaviercd <xaviercd001@protonmail.com>
2017-02-05 22:33:26 +0800

    with Jinja templates

--- headlines.py
+++ headlines.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 import feedparser
 
 RSS_FEEDS = {
@@ -6,7 +7,7 @@ RSS_FEEDS = {
     "cnn": r"http://rss.cnn.com/rss/edition.rss",
     "iol": r"http://www.iol.co.za/cmlink/1.640",
     "fox": r"http://feeds.foxnews.com/foxnews/latest",
-    "sina": r"http://rss.sina.com.cn/sports/global/focus.xml",
+    "sina": r"http://rss.sina.com.cn/news/china/focus15.xml",
 }
 
 
@@ -15,20 +16,9 @@ app = Flask(__name__)
 
 @app.route("/")
 @app.route("/<publication>")
-def get_news(publication="bbc", encoding="utf8"):
+def get_news(publication="bbc"):
     feed = feedparser.parse(RSS_FEEDS[publication])
-    first_article = feed["entries"][0]
-    title = first_article.get("title", "").encode(encoding)
-    published = first_article.get("published", "").encode(encoding)
-    summary = first_article.get("summary", "").encode(encoding)
-    return """<html>
-        <body>
-            <h1> {0} Headlines </h1>
-            <b>{1}</b> <br/>
-            <i>{2}</i> <br/>
-            <p>{3}</p> <br/>
-        </body>
-    </html>""".format(publication.upper(), title, published, summary)
+    return render_template("home.html", articles=feed["entries"])
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
